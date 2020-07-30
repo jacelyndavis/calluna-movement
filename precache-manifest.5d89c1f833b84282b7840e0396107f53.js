dd589d972060212b11b490dc96197aeb06dc9269
jacelyndavis <jacelynb.davis@gmail.com>
2020-07-30 09:56:31 -0700

    Updates

---
+++ precache-manifest.5d89c1f833b84282b7840e0396107f53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa9b4de0fbb95b4cfcff9d368b09bfc",
+    "url": "/calluna-movement/index.html"
+  },
+  {
+    "revision": "a9d62efb8930c5fead80",
+    "url": "/calluna-movement/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95bb9c6748009b22d37a",
+    "url": "/calluna-movement/static/js/2.cf09c1bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calluna-movement/static/js/2.cf09c1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d62efb8930c5fead80",
+    "url": "/calluna-movement/static/js/main.d023b69b.chunk.js"
+  },
+  {
+    "revision": "3a96f3fbdd2e966f0e69",
+    "url": "/calluna-movement/static/js/runtime-main.c2200141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calluna-movement/static/media/logo.5d5d9eef.svg"
+  }
+]);
